e73141d77b6bc9405dd4586dc8b260763f0f1ca7
Ayushi V. <119906759+Ayushivvv@users.noreply.github.com>
2024-12-01 05:11:20 -0500

    Update: fixed the issue of repeated code

--- src/main/java/use_cases/Drawing/auto_save/UndoRedo.java
+++ src/main/java/use_cases/Drawing/auto_save/UndoRedo.java
@@ -4,29 +4,6 @@ import view.*;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
-
-public class UndoRedo {
-    private JFileChooser fileChooser = new JFileChooser();
-
-    public void actionOnCanvas(){
-
-    }
-
-    public JFileChooser getFileChooser() {
-        return fileChooser;
-    }
-
-    public void setFileChooser(JFileChooser fileChooser) {
-        this.fileChooser = fileChooser;
-    }
-}
-package use_cases.Drawing.auto_save;
-
-import view.*;
-
-import javax.swing.*;
-import java.awt.*;
 import java.io.File;
 import java.util.ArrayList;
 
