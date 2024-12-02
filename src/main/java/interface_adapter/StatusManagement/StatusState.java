d1cfa0e1540e42c143c0e79de8bbeca531bcbd56
ayushi <ayushi.verma@mail.utoronto.ca>
2024-12-02 05:29:49 -0500

    Updated StatusState

--- src/main/java/interface_adapter/StatusManagement/StatusState.java
+++ src/main/java/interface_adapter/StatusManagement/StatusState.java
@@ -1,6 +1,26 @@
 package interface_adapter.StatusManagement;
 
+import interface_adapter.Drawing.DrawingState;
+
 import java.awt.image.RenderedImage;
 
 public class StatusState {
+    private static RenderedImage drawing;
+    private String error;
+
+    public static RenderedImage getDrawing() {
+        return drawing;
+    }
+
+    public void setDrawing(RenderedImage drawing) {
+        StatusState.drawing = drawing;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
 }
